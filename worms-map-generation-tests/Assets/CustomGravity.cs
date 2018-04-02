78198be81a57c96331be84b83704a4bf31db8ae1
Nyctef <nyctef@nyctef.com>
2018-04-02 17:31:12 +0100

    Add a custom gravity component
    since 1px = 1unit is way too big of a world scale for what the 3D physics expects

---
+++ worms-map-generation-tests/Assets/CustomGravity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CustomGravity : MonoBehaviour
+{
+
+    public float gravityScale = 1.0f;
+
+    public static float globalGravity = -9.81f;
+
+    Rigidbody _rb;
+
+    void OnEnable()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        _rb.AddForce(gravity, ForceMode.Acceleration);
+    }
+}
