4de97a20cbb8da303e3099a8c7a57a0c896fc7ef
Nyctef <nyctef@nyctef.com>
2018-04-06 10:48:05 +0100

    zoom in the camera and do a simple player follow

---
+++ worms-map-generation-tests/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform Target;
+    public float Distance;
+
+    void Update()
+    {
+        if (Target == null)
+        {
+            Debug.LogError("FollowPlayer no player to follow");
+            return;
+        }
+
+        // just do a really simple follow for now
+        transform.SetPositionAndRotation(Target.position + Vector3.back * Distance, Quaternion.identity);
+    }
+}
