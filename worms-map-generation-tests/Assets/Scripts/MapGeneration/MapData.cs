1aa91c7e90b25ca356814cbe32c8927071397aa8
Nyctef <nyctef@nyctef.com>
2018-04-22 20:33:23 +0100

    Fix gaps between chunks
    overlap chunks by one pixel because marching squares produces a mesh with a size one less than the number of input nodes

--- worms-map-generation-tests/Assets/Scripts/MapGeneration/MapData.cs
+++ worms-map-generation-tests/Assets/Scripts/MapGeneration/MapData.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class MapData
 {
+    private const int ChunkOffset = MapChunk.ChunkSize - 1;
     public MapChunk[] MapChunks;
     public int Width;
     public int Height;
@@ -15,34 +17,58 @@ public class MapData
     {
         Width = width;
         Height = height;
-        ChunksWide = width / MapChunk.ChunkSize + (width % MapChunk.ChunkSize > 0 ? 1 : 0);
-        ChunksHigh = height / MapChunk.ChunkSize + (height % MapChunk.ChunkSize > 0 ? 1 : 0);
+        ChunksWide = width / ChunkOffset + (width % ChunkOffset > 0 ? 1 : 0);
+        ChunksHigh = height / ChunkOffset + (height % ChunkOffset > 0 ? 1 : 0);
 
         MapChunks = new MapChunk[ChunksWide * ChunksHigh];
         for (var x = 0; x < ChunksWide; x++)
         for (var y = 0; y < ChunksHigh; y++)
         {
-            var i = y * ChunksWide + x;
-            MapChunks[i] = new MapChunk();
-            MapChunks[i].Init(x * MapChunk.ChunkSize, y * MapChunk.ChunkSize);
+            var chunkIndex = y * ChunksWide + x;
+            MapChunks[chunkIndex] = new MapChunk();
+            Debug.Log("Init new chunk at " + x * ChunkOffset + ", " + y * ChunkOffset);
+            MapChunks[chunkIndex].Init(x * ChunkOffset, y * ChunkOffset);
         }
     }
 
     public bool Get(int x, int y)
     {
-        var chunkX = x / MapChunk.ChunkSize;
-        var chunkY = y / MapChunk.ChunkSize;
-        var pixelX = x % MapChunk.ChunkSize;
-        var pixelY = y % MapChunk.ChunkSize;
-        return (MapChunks[chunkY * ChunksWide + chunkX].Chunk[pixelY] & 1UL << pixelX) > 0;
+        var chunkX = x / ChunkOffset;
+        var chunkY = y / ChunkOffset;
+        var pixelX = x % ChunkOffset;
+        var pixelY = y % ChunkOffset;
+        var chunkIndex = chunkY * ChunksWide + chunkX;
+        return (MapChunks[chunkIndex].Chunk[pixelY] & 1UL << pixelX) > 0;
     }
 
     public void Set(int x, int y, bool value)
     {
-        var chunkX = x / MapChunk.ChunkSize;
-        var chunkY = y / MapChunk.ChunkSize;
-        var pixelX = x % MapChunk.ChunkSize;
-        var pixelY = y % MapChunk.ChunkSize;
+        var chunkX = x / ChunkOffset;
+        var chunkY = y / ChunkOffset;
+        var pixelX = x % ChunkOffset;
+        var pixelY = y % ChunkOffset;
+        SetOnChunk(value, chunkX, chunkY, pixelX, pixelY);
+
+        if (pixelX == 0 && chunkX > 0)
+        {
+            SetOnChunk(value, chunkX - 1, chunkY, 63, pixelY);
+        }
+        if (pixelX == ChunkOffset && chunkX < ChunksWide - 1)
+        {
+            SetOnChunk(value, chunkX + 1, chunkY, 0, pixelY);
+        }
+        if (pixelY == 0 && chunkY > 0)
+        {
+            SetOnChunk(value, chunkX, chunkY - 1, pixelX, 63);
+        }
+        if (pixelY == ChunkOffset && chunkY < ChunksHigh - 1)
+        {
+            SetOnChunk(value, chunkX, chunkY + 1, pixelX, 0);
+        }
+    }
+
+    private int SetOnChunk(bool value, int chunkX, int chunkY, int pixelX, int pixelY)
+    {
         var chunkIndex = chunkY * ChunksWide + chunkX;
         ChangedChunkIndexes.Add(chunkIndex);
         if (value)
@@ -53,5 +79,6 @@ public class MapData
         {
             MapChunks[chunkIndex].Chunk[pixelY] &= ~(1UL << pixelX);
         }
+        return chunkIndex;
     }
 }
