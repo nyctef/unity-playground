1be8363bba32a9a5c5b43c403000adf475fa4126
Nyctef <nyctef@nyctef.com>
2018-04-27 16:44:47 +0100

    Add an explosion force on other rigidbodies

--- worms-map-generation-tests/Assets/Scripts/ExplosionManager.cs
+++ worms-map-generation-tests/Assets/Scripts/ExplosionManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 
 public class ExplosionManager : MonoBehaviour
 {
@@ -19,5 +20,22 @@ public class ExplosionManager : MonoBehaviour
         // TODO add some particles and other effects
         // TODO cope with different explosion sizes
         Instantiate(Explosion50Prefab, explosion.WorldSpacePosition, Quaternion.identity);
+
+        var explosionTargets = Physics.OverlapSphere(explosion.WorldSpacePosition, 50);
+        Debug.Log("Explosion affecting "+explosionTargets.Length+ " targets");
+        foreach (var h in explosionTargets)
+        {
+            var r = h.GetComponent<Rigidbody>();
+            if (r != null)
+            {
+                r.AddExplosionForce(1000, explosion.WorldSpacePosition, 50, 10, ForceMode.Impulse);
+            }
+
+            var cc = h.GetComponent<CharacterController>();
+            if (cc != null)
+            {
+                ; // todo move character
+            }
+        }
     }
 }
