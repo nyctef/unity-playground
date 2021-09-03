0363e2cc61340e72b685881a2925f5aa88f9050c
nyctef <nyctef@nyctef.com>
2021-09-03 17:26:10 +0100

    implement the rest of MouseLook.cs

--- monochrome-maze/Assets/MouseLook.cs
+++ monochrome-maze/Assets/MouseLook.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
+    [Range(1f, 1000f)]
+    public float mouseSensitivity = 100f;
+
+    public Transform player;
+
+    private float xRotation = 0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // TODO: look into using the new input system?
+        var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        player.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -89f, 89f);
+
+        // we apply the different axes to different objects: mouseY (left/right) actually rotates
+        // the parent player object, while mouseX (up/down) just rotates the camera (this transform)
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        player.Rotate(Vector3.up * mouseY);
     }
 }
