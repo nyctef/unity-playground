46832f7530d6a7b212c2195e9def688cd601ce3d
nyctef <nyctef@nyctef.com>
2021-09-04 08:51:15 +0100

    clear up some confusion in MouseLook.cs

--- monochrome-maze/Assets/MouseLook.cs
+++ monochrome-maze/Assets/MouseLook.cs
@@ -9,7 +9,7 @@ public class MouseLook : MonoBehaviour
 
     public Transform player;
 
-    private float xRotation = 0f;
+    private float upDownRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,14 +24,23 @@ public class MouseLook : MonoBehaviour
         var mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         var mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        player.Rotate(Vector3.up * mouseX);
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -89f, 89f);
+        upDownRotation += mouseY;
+        upDownRotation = Mathf.Clamp(upDownRotation, -89f, 89f);
 
         // we apply the different axes to different objects: mouseY (left/right) actually rotates
-        // the parent player object, while mouseX (up/down) just rotates the camera (this transform)
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        player.Rotate(Vector3.up * mouseY);
+        // the parent player object, while mouseY (up/down) just rotates the camera (this transform)
+
+        // up/down rotation translates into rotation around the x axis.
+        //
+        // when the player moves the mouse up, we get a positive mouseY value.
+        // apparently unity uses a "left-handed" coordinate system, so the x axis
+        // points to the right and rotations go counterclockwise when viewed in the x axis direction.
+        // this means a positive rotation around the x axis causes the camera to look down, and we 
+        // need to negate this value:
+        transform.localRotation = Quaternion.Euler(-upDownRotation, 0f, 0f);
+
+        // left/right rotation is simpler: we just ask the character controller to rotate the player
+        // around the y axis
+        player.Rotate(Vector3.up * mouseX);
     }
 }
